c0e8ab3102d44e7b848d6536c7731e5b6af55438
Aleksandr <arrarrat65@gmail.com>
2023-04-17 08:20:00 +0400

    fix:

--- js/module/ClassMiddleWIFIesp8266.js
+++ js/module/ClassMiddleWIFIesp8266.js
@@ -17,6 +17,15 @@
  * @property {number} signal           - уровень сигнала в децибелах
  * @property {string} mac              - Mac-адрес устройства точки доступа
  * 
+ * Тип для хранения опций при передаче сообщений по HTTP
+ * @typedef  {Object} ObjectHTTPOpts   - тип контейнер хранящий опции
+ * @property {string} host             - адрес хоста
+ * @property {number} port             - номер порта (не обязателен, по-умолчанию -- 80)
+ * @property {string} path             - путь на сервере
+ * @property {string} method           - тип запроса (GET, POST...)
+ * @property {string} protocol         - тип протокола (не обязателен)
+ * @property {Object} headers          - хедеры (не обязательны)
+ * 
  * Методы аутентификации следующие: open, wep, wpa_psk, wpa2_psk, wpa_wpa2_psk
  */
 class ClassEsp8266 {
@@ -24,10 +33,26 @@ class ClassEsp8266 {
      * @constructor
      * @param {Object} _Bus   - - объект класса UARTBus
      */
-    constructor() {
+    constructor(_Bus) {
         this.name = 'ClassEsp8266'; //переопределяем имя типа
-        this.wifi = require('https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js').setup(_Bus, callback);
+        this.wifi = undefined;
+        this.ecode = 12;
+        this.Init(_Bus);
 	}
+    /**
+     * Метод начальной инициализации выкидывает ошибку, определенную в базовом классе,
+     * если не удалось установить соединение
+     * @param {Object} _Bus   - - объект класса UARTBus
+     */
+    Init(_Bus)
+    {
+        let req = 'https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js';
+        this.wifi = require(req).setup(_Bus, function(emsg) {
+            if (emsg) {
+                throw new err (emsg, this.ecode);
+            }
+        });
+    }
     /**
      * @method
      * Создаёт новое подключение к уже
@@ -36,15 +61,36 @@ class ClassEsp8266 {
      * @param {string} _pass  - пароль сети
      */
     Connect(_ssid, _pass) {
-        this.wifi.connect(_ssid, _pass, function(err) {
-            print('Connected');
-            // Выполняем запрос - для проверки
-            require('http').get('http://amperka.ru', function(res) {
-              var response = '';
-              res.on('data', function(d) { response += d; });
-              res.on('close', function() { print(response); });
+        this.wifi.connect(_ssid, _pass, function(emsg) {
+            if (emsg) {
+                throw new err (emsg, this.ecode);
+            }
+          });
+    }
+    /**
+     * @method
+     * Осуществляет отправку сообщений по сети с
+     * использованием протокола HTTP
+     * @param {ObjectHTTPOpts} _opts    - параметры соединения
+     * @param {string} _body            - тело запроса (для POST должно быть не пустым)
+     * @returns {string} ret_data       - информация о результате запроса
+     */
+    HttpRequest(_opts, _body) {
+        let ret_data;
+        let req = require("http").request(options, function(res) {
+            res.on('data', function(data) {
+                ret_data += data;
+            });
+            res.on('close', function() {
+                ret_data += "\nConnection closed\n";
             });
           });
+          if (_body && _opts.method == 'POST') {
+            req.write(_body);
+            ret_data += "\nPOST complete\n";
+          }
+          req.end();
+          return ret_data;
     }
     /**
      * @method
@@ -54,7 +100,10 @@ class ClassEsp8266 {
      */
     GetIP() {
         let _ip;
-        this.wifi.getIP(function (err, ipAdress) {
+        this.wifi.getIP(function (emsg, ipAdress) {
+            if (emsg) {
+                throw new err (emsg, this.ecode);
+            }
             _ip = ipAdress;
         });
         return _ip;
@@ -62,11 +111,14 @@ class ClassEsp8266 {
     /**
      * @method
      * Возвращает информацию о найденных точках доступа
-     * @returns {ObjectAP} _aps  -найденные точки доступа
+     * @returns {ObjectAP} _aps  - найденные точки доступа
      */
     GetAPs() {
         let _aps;
-        this.wifi.getAPs(function (err, aps) {
+        this.wifi.getAPs(function (emsg, aps) {
+            if (emsg) {
+                throw new err (emsg, this.ecode);
+            }
             _aps = aps;
         });
         return _aps;
@@ -74,12 +126,14 @@ class ClassEsp8266 {
     /**
      * @method
      * Возвращает версию прошивки модуля
-     * @returns {string} _ver  -Версия прошивки
+     * @returns {string} _ver  - версия прошивки
      */
     GetVersion() {
         let _ver;
-        wifi.getVersion(function(err, version) {
-            // Debug
+        this.wifi.getVersion(function(emsg, version) {
+            if (emsg) {
+                throw new err (emsg, this.ecode);
+            }
             _ver = version;
           });
         return _ver;
@@ -92,22 +146,30 @@ class ClassEsp8266 {
      * @param {string}  _pass - пароль создаваемой сети
      * @param {number}  _chan - номер радиоканала
      * @param {string}  _enc  - метод аутентификации
+     * @returns {string} _res - сообщение о создании сети
      */
     CreateAP(_ssid, _pass, _chan, _enc) {
-        this.wifi.createAP(_ssid, _pass, _chan, _enc, function(err) {
-            print('AP created');
+        let _res = 'AP created';
+        this.wifi.createAP(_ssid, _pass, _chan, _enc, function(emsg) {
+            if (emsg) {
+                throw new err (emsg, this.ecode);
+            }
         });
+        return _res;
     }
     /**
      * @method
      * Возвращает список подключенных устройств
      * в виде массива объектов. Каждый объект содержит
      * поля IP и Mac-адрессов
-     * @returns {ObjectDevice} _devs -подключенные устройства
+     * @returns {ObjectDevice} _devs - подключенные устройства
      */
     GetConnectedDevices() {
         let _devs;
-        this.wifi.getConnectedDevices (function (err, devices) {
+        this.wifi.getConnectedDevices (function (emsg, devices) {
+            if (emsg) {
+                throw new err (emsg, this.ecode);
+            }
             _devs = devices;
         });
         return _devs;
