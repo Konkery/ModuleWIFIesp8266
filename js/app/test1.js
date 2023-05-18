969463c2794ccb547af8561e417d40db8dd0174a
Aleksandr <arrarrat65@gmail.com>
2023-05-18 14:06:24 +0400

    fix:

--- js/app/test1.js
+++ js/app/test1.js
@@ -6,17 +6,24 @@ try {
     let wifi = require("https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js").setup(Serial3, function (err) {
         if (err) {
             console.log('Module connection error! ' + err)
-        }     
+        }
+      Scan();
+      //let scan = setInterval(() => Scan(), 5000);
     });
 
-    wifi.getAPs(function(err, aps) {
-        if (err) {
-            console.log('Error looking for APs: ' + err)
-        }
-        else {
-            console.log(aps);
-        }
-    });
+     //
+     function Scan() {
+        wifi.getAPs(function(err, aps) {
+          if (err) {
+              console.log('Error looking for APs: ' + err)
+          }
+          else {
+              let res = aps.map(a => a.ssid)
+              console.log(res);
+          }
+        });
+   
+  }
   }
   catch(e) {
     console.log('Error!');
