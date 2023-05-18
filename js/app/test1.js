9efb2791a9a0d2d07ad77d96229661e29a3ca022
Aleksandr <arrarrat65@gmail.com>
2023-05-18 13:58:32 +0400

    fix:

--- js/app/test1.js
+++ js/app/test1.js
@@ -2,23 +2,21 @@ try {
     Serial3.setup(115200);
     let SSID = '';
     let PSWD = '';
-    //https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js
     
-    let wifi = require("https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js").setup(Serial3, function (errs) {
-      wifi.connect(SSID, PSWD, function(err) {
-      print('Connected');
-        require('http').get('http://amperka.ru', function(res) {
-        var response = '';
-        res.on('data', function(d) { response += d; });
-        res.on('close', function() { print(response); });
-      });
-      });
-     wifi.getIP(function(errs, ip) {
-      if (errs) print('error on reset:', errs);
-      console.log(ip);
+    let wifi = require("https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js").setup(Serial3, function (err) {
+        if (err) {
+            console.log('Module connection error! ' + err)
+        }     
     });
-      wifi.getAPs(function(err, aps) { console.log(aps); });
-  });
+
+    wifi.getAPs(function(err, aps) {
+        if (err) {
+            console.log('Error looking for APs: ' + err)
+        }
+        else {
+            console.log(aps);
+        }
+    });
   }
   catch(e) {
     console.log('Error!');
--- js/app/test2.js
+++ js/app/test2.js
@@ -1,9 +1,17 @@
-/*{
-            path: '/',
-            port: 8080,
-            protocol : "echo-protocol",
-            protocolVersion: 13,
-            origin: 'Espruino',
-            keepAlive: 60,
-            headers:{ some:'header', 'ultimate-question':42 }
-            }*/
+try {
+    let WS = require("https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassWebSocketClient.min.js");
+    let opts = {
+        path: '/',
+        port: 8080,
+        protocol : "echo-protocol",
+        protocolVersion: 13,
+        origin: 'Espruino',
+        keepAlive: 60,
+        headers:{ some:'header', 'ultimate-question':42 }
+        };
+    let socket = new WS ("www.google.com",opts);
+    socket.InvCallBack('ping');
+}
+catch (e) {
+    console.log('Error!');
+}
