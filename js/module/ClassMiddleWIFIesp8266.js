41af031703121096bedb02ba231a24cea404d97f
Aleksandr <arrarrat65@gmail.com>
2023-06-28 13:57:47 +0400

    fix:

--- js/module/ClassMiddleWIFIesp8266.js
+++ js/module/ClassMiddleWIFIesp8266.js
@@ -116,25 +116,25 @@ class ClassEsp8266WiFi {
         }
         else {
             this.wifi = require("Wifi");
-            console.log("Hello blyat!");
+            console.log("Hello");
             /*this.wifi.connect('Gorizont-Zero', { password : "gorizont#zero" }, function(err) {
             if (err) {
                 console.log("Connection error: "+err);
                 return;
             }*/
             this.wifi.scan((err, ap_list) => {
-                if (err !== null) {
+                if (err) {
+                    console.log("Connection error: "+err);
                     throw err;
                   }
                   console.log(ap_list);
             });
             this.wifi.getIP((err, info) => {
-                if (err !== null) {
+                if (err) {
                   throw err;
                 }
                 console.log("IP: "+info.ip);            
             });
-        //});
         }
     }
     /**
