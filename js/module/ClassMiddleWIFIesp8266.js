ef7b8f21f78838b6020d3f0d0c3f9b275a379737
Aleksandr <arrarrat65@gmail.com>
2023-07-08 16:56:18 +0400

    fix: stable version

--- js/module/ClassMiddleWIFIesp8266.js
+++ js/module/ClassMiddleWIFIesp8266.js
@@ -42,14 +42,12 @@ class ClassEsp8266WiFi {
             ClassEsp8266WiFi.prototype.Instance = this;
         }
 
-        this.name = 'ClassEsp8266WiFi'; //переопределяем имя типа
+        this._name = 'ClassEsp8266WiFi'; //переопределяем имя типа
         this._wifi = undefined;
-        this.ssid = undefined;
-        this.pass = undefined;
-        //this.bus = undefined;
-        //this.InitBus(_rx, _tx);
-        this.ScanForAPs();
-        //this.Connect();
+        this._bus = undefined;
+        this._ssid = undefined;
+        this._ip = undefined;
+        this.Init(_rx, _tx);
 	}
     /**
      * @method
@@ -61,19 +59,58 @@ class ClassEsp8266WiFi {
         if (_rx && _tx) {
             let bus_class = new UARTBus();
             let opt = {rx: _rx, tx: _tx, baud: 115200};
-            this.bus = bus_class.AddBus(opt);
+            this._bus = bus_class.AddBus(opt);
         }
     }
     /**
      * @method
      * Сканирует окружение на наличие точек доступа и выбирает
      * из знакомых к какой подключится, и осуществляет подключение
+     * @param {Object} _rx      - порт rx шины UART, нужен для запуска шины
+     * @param {Object} _tx      - порт tx шины UART, нужен для запуска шины
      */
-    ScanForAPs() {
-        // функции те-жеб реквайр другой - как определить модуль, на котором мы работаем?
-        if (process.env.BOARD === "ISKRAJS") {
-            Serial3.setup(115200);
-            this._wifi = require("https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js").setup(Serial3, (err) => {
+    Init(_rx, _tx) {
+        if (process.env.MODULES.includes("Wifi")) {
+            this._wifi = require("Wifi");
+            let pass;
+            this._wifi.scan((err, aps) => {
+                if (err) {
+                    throw err;
+                  }
+                  else {
+                    let s = require("Storage");
+                    if (!(s.list().includes("APs.json"))) {
+                        throw "No JSON file found!";
+                    }
+                    let found = aps.map(a => a.ssid);
+                    let wrt = s.readJSON("APs.json", true);
+
+                    found.forEach(fName => {
+                        wrt.forEach(sName => {
+                            if (fName == sName.ssid) {
+                                this._ssid = sName.ssid;
+                                pass = sName.pass;
+                            }                               
+                        });
+                    });
+                    this._wifi.connect(this.ssid, { password : pass }, (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                        this._wifi.getIP((err, info) => {
+                            if (err) {
+                              throw err;
+                            }
+                           this._ip = info.ip;            
+                        });
+                    });
+                }
+            });
+        }
+        else {
+            this.InitBus(_rx, _tx);
+            let pass;
+            this._wifi = require("https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js").setup(this._bus, (err) => {
                 if (err) {
                     throw err;
                 }
@@ -82,198 +119,38 @@ class ClassEsp8266WiFi {
                         throw err;
                     }
                     else {
+                        let s = require("Storage");
+                        if (!(s.list().includes("APs.json"))) {
+                            throw "No JSON file found!";
+                        }
                         let found = aps.map(a => a.ssid);
-                        let wrt = require("Storage").readJSON("APs.json", true);
-                        //console.log (found);
+                        let wrt = s.readJSON("APs.json", true);
 
                         found.forEach(fName => {
                             wrt.forEach(sName => {
                                 if (fName == sName.ssid) {
-                                    this.ssid = sName.ssid;
-                                    this.pass = sName.pass;
+                                    this._ssid = sName.ssid;
+                                    pass = sName.pass;
                                 }                               
                             });
                         });
-                        this.ssid = 'Gorizont-Zero';
-                        this.pass = 'gorizont#zero';
-                        this._wifi.connect (this.ssid, this.pass, (err) => {
+                        this._wifi.connect (this._ssid, pass, (err) => {
                             if (err) {
-                                console.log(this.ssid + '\nConnection failed! ' + err);
                                 throw err;
                             }
                             else {
-                                // Бип! - добавить метод на писк бипера
                                 this._wifi.getIP((emsg, ipAdress) => {
                                     if (emsg) {
                                         throw err;
                                     }
-                                    console.log("IP: " + ipAdress);
+                                    this._ip = ipAdress;
                                 });
-                                //this.AddToList();
                             }
                         });
                     }
                 });                
             });
-        }
-        else {
-            this._wifi = require("Wifi");
-            console.log("Hello345");
-            this._wifi.connect('Gorizont-Zero', { password : "gorizont#zero" }, (err) => {
-                if (err) {
-                    console.log("Connection error: "+err);
-                    return;
-                }
-                this._wifi.getIP((err, info) => {
-                    if (err) {
-                      throw err;
-                    }
-                    console.log("IP: "+info.ip);            
-                });
-            });
-           /* this.wifi.scan((err, ap_list) => {
-                if (err) {
-                    console.log("Connection error: "+err);
-                    throw err;
-                  }
-                  console.log(ap_list);
-            });*/            
-        }
-    }
-    /**
-     * @method
-     * Создаёт новое подключение к уже
-     * существующей WiFi-сети
-     */
-    Connect() {
-        if (process.env.BOARD === "ISKRAJS") {
-            let wifi = require("https://raw.githubusercontent.com/AlexGlgr/ModuleMiddleWIFIesp8266/fork-Alexander/js/module/ClassBaseWIFIesp8266.min.js").setup(Serial3, function (err) {
-                if (err) {
-                    console.log('Module connection error! ' + err);
-                }
-                wifi.connect (this.ssid, this.pass, function (err) {
-                    if (err) {
-                        console.log(this.ssid + " " +  this.pass + '\nConnection failed! ' + err);
-                    }
-                    else {
-                        console.log('Connected to ' + this.ssid);
-                        // Бип! - добавить метод на писк бипера
-                        wifi.getIP(function (emsg, ipAdress) {
-                            if (emsg) {
-                                throw new err (emsg, this.ecode);
-                            }
-                            console.log("IP: " + ipAdress);
-                        });
-                        //this.wifi = wifi;
-                        //this.AddToList();
-                    }
-                })            
-            });
-        }
-    }
-    /**
-     * @method
-     * Метод добавляет текущее подключение в конфигурационный файл,
-     * если сети там ещё нет
-     */
-    AddToList() {
-        let i = 0;
-        let flag = false;
-        let wrt = require("Storage").readJSON("APs.json", true);
-        for (i; i < wrt.length; i++) {
-            if (this.ssid == wrt[i].ssid) {
-                flag = true;
-                break;
-                }
-            }
-        if (!flag) {
-            wrt.push({ssid: this.ssid, pass: this.pass});
-            require("Storage").writeJSON("APs.json", wrt); 
-        }
-    }
-    /**
-     * @method
-     * Возвращает IP адрес, полученный модулем от
-     * точки доступа при подключении
-     * @returns {Object} _ip   -ip адрес
-     */
-    GetIP() {
-        let _ip;
-        this.wifi.getIP(function (emsg, ipAdress) {
-            if (emsg) {
-                throw new err (emsg, this.ecode);
-            }
-            _ip = ipAdress;
-        });
-        return _ip;
-    }
-    /**
-     * @method
-     * Возвращает информацию о найденных точках доступа
-     * @returns {ObjectAP} _aps  - найденные точки доступа
-     */
-    GetAPs() {
-        let _aps;
-        this.wifi.getAPs(function (emsg, aps) {
-            if (emsg) {
-                throw new err (emsg, this.ecode);
-            }
-            _aps = aps;
-        });
-        console.log(_aps);
-        return _aps;
-    }
-    /**
-     * @method
-     * Возвращает версию прошивки модуля
-     * @returns {string} _ver  - версия прошивки
-     */
-    GetVersion() {
-        let _ver;
-        this.wifi.getVersion(function(emsg, version) {
-            if (emsg) {
-                throw new err (emsg, this.ecode);
-            }
-            _ver = version;
-          });
-        return _ver;
-    }
-    // проверить какой ip
-    /**
-     * @method
-     * Создаёт WiFi-сеть. Модуль начинает работать в
-     * режиме точки доступа
-     * @param {string}  _ssid - ssid создаваемой сети
-     * @param {string}  _pass - пароль создаваемой сети
-     * @param {number}  _chan - номер радиоканала
-     * @param {string}  _enc  - метод аутентификации
-     * @returns {string} _res - сообщение о создании сети
-     */
-    CreateAP(_ssid, _pass, _chan, _enc) {
-        let _res = 'AP created';
-        this.wifi.createAP(_ssid, _pass, _chan, _enc, function(emsg) {
-            if (emsg) {
-                throw new err (emsg, this.ecode);
-            }
-        });
-        return _res;
-    }
-    /**
-     * @method
-     * Возвращает список подключенных устройств
-     * в виде массива объектов. Каждый объект содержит
-     * поля IP и Mac-адрессов
-     * @returns {ObjectDevice} _devs - подключенные устройства
-     */
-    GetConnectedDevices() {
-        let _devs;
-        this.wifi.getConnectedDevices (function (emsg, devices) {
-            if (emsg) {
-                throw new err (emsg, this.ecode);
-            }
-            _devs = devices;
-        });
-        return _devs;
+        }            
     }
 }
 
